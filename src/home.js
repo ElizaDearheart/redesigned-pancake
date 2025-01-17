c346cebaccec22b59f90c345e52c207ce16914f1
Jordan Keller <keller.jc@gmail.com>
2025-01-17 15:03:31 -0800

    Add home page content

---
+++ src/home.js
@@ -0,0 +1,59 @@
+import tulip from "./images/tulip.jpg";
+
+function Home() {
+  //mainContentDiv
+  let mainContentDiv;
+  mainContentDiv = document.getElementById("content");
+  // home page content
+  let homePageContent;
+  homePageContent = document.createElement("div");
+  homePageContent.className = "home-page";
+  mainContentDiv.appendChild(homePageContent);
+  // tulip div
+  let tulipDiv = document.createElement("div");
+  tulipDiv.className = "tulip-div";
+  const tulipImage = document.createElement("img");
+  tulipImage.src = tulip;
+  tulipDiv.appendChild(tulipImage);
+  homePageContent.appendChild(tulipDiv);
+  // info div
+  let infoDiv = document.createElement("div");
+  infoDiv.className = "info-div";
+  homePageContent.appendChild(infoDiv);
+  // infoDiv content
+  let textOne = document.createElement("p");
+  textOne.className = "text-one";
+  textOne.textContent = "Each plate made to your exact specifications.";
+  let textTwo = document.createElement("p");
+  textTwo.className = "text-two";
+  textTwo.textContent =
+    "A canvas waiting to be filled with your dream breakfast...";
+  infoDiv.appendChild(textOne);
+  infoDiv.appendChild(textTwo);
+  // infoDiv form element
+  let signupForm = document.createElement("form");
+  signupForm.className = "sign-up";
+  infoDiv.appendChild(signupForm);
+
+  let newEmail = document.createElement("input");
+  newEmail.type = "email";
+  newEmail.name = "newEmail";
+  newEmail.id = "newEmail";
+  newEmail.placeholder = "Enter your email here...";
+
+  let newEmailLabel = document.createElement("label");
+  newEmailLabel.textContent = "Sign up to receive news and offers:";
+
+  signupForm.appendChild(newEmailLabel);
+  signupForm.appendChild(newEmail);
+
+  const newLineBreak = document.createElement("br");
+  signupForm.appendChild(newLineBreak);
+
+  let submitButton = document.createElement("button");
+  submitButton.className = "submit";
+  submitButton.textContent = "Submit";
+  signupForm.appendChild(submitButton);
+}
+
+Home();
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import "./styles.css";
+import "./home.js";
+import "./menu.js";
+import "./about.js";
+
+console.log("hello");
